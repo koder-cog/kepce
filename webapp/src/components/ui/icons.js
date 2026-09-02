f2847640e7d69bb49c3ad88c34ae6b885ddc8ecb
koder-cog <224581822+koder-cog@users.noreply.github.com>
2026-09-02 20:27:04 +0300

    feat(icons): rss.svg objects altina tasindi ve icons.js uzerinden tanimlandi

--- webapp/src/components/ui/icons.js
+++ webapp/src/components/ui/icons.js
@@ -123,6 +123,7 @@ import system from '../../assets/icons/objects/system.svg?raw';
 import trophy from '../../assets/icons/objects/trophy.svg?raw';
 import usage from '../../assets/icons/objects/usage.svg?raw';
 import user from '../../assets/icons/objects/user.svg?raw';
+import rss from '../../assets/icons/objects/rss.svg?raw';
 
 import { dev, building } from '$app/environment';
 
@@ -250,7 +251,7 @@ export const icons = {
   refresh,
   redo,
   copy,
-  rss: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 11a9 9 0 0 1 9 9"></path><path d="M4 4a16 16 0 0 1 16 16"></path><circle cx="5" cy="19" r="1"></circle></svg>',
+  rss,
 };
 
 /**
